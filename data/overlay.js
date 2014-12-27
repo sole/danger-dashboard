5d908897780bd778fdd9b58304c8c6c7766fa026
sole <listas@soledadpenades.com>
2014-12-27 11:25:08 +0100

    move CSS to another file

--- data/overlay.js
+++ data/overlay.js
@@ -1,15 +1,8 @@
 // This script will be run each time a page is loaded
 
-// Inspired by Dr. Jenn Schiffer
-// https://twitter.com/jennschiffer/status/402822075317878784
-var toTheFuckingMoon = '100000';
-
 var div = document.createElement('div');
 div.innerHTML = 'danger dashboard';
+div.id = 'danger-dashboard';
 
 document.body.appendChild(div);
 
-div.style.position = 'absolute';
-div.style.right = '0px';
-div.style.bottom = '0px';
-div.style.zIndex = toTheFuckingMoon;
